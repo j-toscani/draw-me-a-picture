2490663f4e9742c899d1906f699951c1aa78d9d0
Julian Toscani <julian.toscani@rtl.com>
2022-11-13 12:50:26 +0100

    add route to join room

--- server/createExpressServer.ts
+++ server/createExpressServer.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import { getRoomState } from "./socketHandlers/handleCreateRoom";
+
+const router = express.Router();
+
+router.get("/rooms/:id", (req, res) => {
+    const {id} = req.params;
+    console.log(id);
+    res.send(getRoomState(id)?.file);
+})
 
 export default function createExpressServer(port = 4000) {
     const app = express();
-    const server = app.listen(port, () => console.log(`App started on port ${port}`));
+    app.use(router);
     app.use(express.static("public"));
+
+    const server = app.listen(port, () => console.log(`App started on port ${port}`));
+
     return {server, app};
 }
 
